bcaf22937110cfe92cb7b693a4b3fba5391a6b84
Roman <galyshev.roman@gmail.com>
2024-02-22 12:45:13 +0200

    v0.3 генерация игровых полей

--- new_game.py
+++ new_game.py
@@ -1,6 +1,14 @@
 from fastapi import APIRouter, Request, Body
 from starlette.templating import Jinja2Templates
+from fastapi import Depends
+from sqlalchemy import select, update, insert
+from sqlalchemy.ext.asyncio import AsyncSession
+from datetime import datetime
 
+from alchemy import get_async_session #ДЛЯ ЗАПУСКА ЧЕРЕЗ КОНСОЛЬ
+from BD.model_bd import player_game_field, comp_game_field   #ДЛЯ ЗАПУСКА ЧЕРЕЗ КОНСОЛЬ
+# from alchemy import get_async_session       #ДЛЯ ЗАПУСКА ЧЕРЕЗ ДОКЕР
+# from ИВ.model_bd import player_game_field, comp_game_field      #ДЛЯ ЗАПУСКА ЧЕРЕЗ ДОКЕР
 
 router = APIRouter(
     prefix='/new',
@@ -10,9 +18,32 @@ router = APIRouter(
 templates = Jinja2Templates(directory="templates")
 
 @router.post('/')
-async def start_new(request: Request):
-    play_btn_class = "play_btn_free"
-    return templates.TemplateResponse('new_game.html', {"request": request, "play_btn_class": play_btn_class})
+async def start_new(request: Request, session: AsyncSession = Depends(get_async_session)):
+    # ГЕНЕРАЦИЯ ИГРОВЫХ ПОЛЕЙ для текущей игры
+    current_datetime = str(datetime.today())
+    vertical_index = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    horizontal_index = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    for v in vertical_index:
+        for h in horizontal_index:
+            stmt_p = insert(player_game_field).values(vert_index=v, horiz_index=h, ship="0", id_ship="0",
+                                                    create_date=current_datetime, field_class='play_btn_free')
+            await session.execute(stmt_p)
+
+            stmt_c = insert(comp_game_field).values(vert_index=v, horiz_index=h, ship="0", id_ship="0",
+                                                  create_date=current_datetime, field_class='play_btn_free')
+            await session.execute(stmt_c)
+            await session.commit()
+
+    query_p = select(player_game_field).where(player_game_field.c.create_date == current_datetime)
+    result = await session.execute(query_p)
+    rez_pl = result.all()
+
+    query_c = select(comp_game_field).where(comp_game_field.c.create_date == current_datetime)
+    result = await session.execute(query_c)
+    rez_comp = result.all()
+
+    return templates.TemplateResponse('new_game.html', {"request": request, "rez_pl": rez_pl, "rez_comp": rez_comp,
+                                                        "v_index": vertical_index})
 
 @router.post('/generate')
 async def generate(request: Request, data:str =Body()):
